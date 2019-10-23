d4c23b91f810cca700b252b3f8ec9c5502f9bab9
Vasiliev Saryal <saryal.basilev@mail.ru>
2019-10-23 18:45:25 +0900

    task1 DONE

--- 1.py
+++ 1.py
@@ -1,1 +1,29 @@
 # Задание 1: права доступа
+perms = []
+
+n = int(input())
+for i in range(n):
+	perm = input().split()
+	try:
+		perm[perm.index('X')] = 'execute'
+	except:
+		continue
+	try:	
+		perm[perm.index('R')] = 'read'
+	except:
+		continue
+	try:
+		perm[perm.index('W')] = 'write'
+	except:
+		continue
+
+m = int(input())
+for j in range(m):
+	operation, filename = input().split()
+	try:
+		perm.index(operation)
+		perms.append("OK")
+	except:
+		perms.append("Access denied")
+for k in perms:
+	print(k)
