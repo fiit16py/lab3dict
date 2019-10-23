c5e584df1d3ba18adb64802e1ae0005f60d1f66e
Fedorov Sergey <FreddyGrey12@gmail.com>
2019-10-23 19:14:40 +0900

    Task 1 Done

--- 1.py
+++ 1.py
@@ -1,1 +1,21 @@
-#ew
+# Задание 1: права доступа
+perms = {}
+
+n = int(input())
+for i in range(n):
+	words = input().split()
+	file = words[0]
+	perms[file] = words[1: ]
+print(perms)
+
+m = int(input())
+opcode = {'read': 'R',
+		  'write': 'W',
+		  'execute': 'X'}
+for i in range(m):
+	operation, file = input().split()
+	code = opcode[operation]
+	if code in perms[file]:
+		print("OK")
+	else:
+		print("Access denied")
