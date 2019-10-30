162a5abc1294f92ca88bdff0866785497a5039f4
LATRio <AlSenya@yandex.ru>
2019-10-30 16:16:56 +0900

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,14 +1,12 @@
-f = open("04")
-file_count = int(f.readline())
+file_count = int(input())
 files = []
 for i in range(file_count):
-    file = f.readline()
-    file_parse = file.split()
+    file_parse = input().split()
     file_info = {"filename":file_parse[0],"permissions":[]}
     file_info["permissions"] = file_parse[1:]
     files.append(file_info)
     pass
-cmd_count = int(f.readline())
+cmd_count = int(input() )
 
 def check_perm(op, file):
     for i in range(file_count):
@@ -19,12 +17,7 @@ def check_perm(op, file):
     return 'Access denied'
 
 for i in range(cmd_count):
-    cmd = f.readline()
-    cmd_parse = cmd.split()
-    if cmd_parse[0] == 'read':
-        print(check_perm("R", cmd_parse[1]))
-    elif cmd_parse[0] == 'write':
-        print(check_perm("W", cmd_parse[1]))
-    elif cmd_parse[0] == 'execute':
-        print(check_perm("X", cmd_parse[1]))
+    conv = {'write':'W', 'read':'R', 'execute':'X'} 
+    cmd_parse = input().split()
+    print(check_perm(conv[cmd_parse[0]], cmd_parse[1]))
     pass
